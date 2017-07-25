080b44ab4248467017308fd13d23608ed7ea4ff6
kabiondi <kellenbiondi@gmail.com>
2017-07-25 13:41:05 -0700

    WIP saving new files for stash

---
+++ portfolio/js/portfolio.js
@@ -0,0 +1,25 @@
+// Animate line script
+var animateLine = function () {
+  $( ".animate-divider" ).animate({
+    opacity: 1,
+    height: "170"
+  }, 1000, function() {
+    // Animation complete.
+  });
+}
+var animateHorizontal = function () {
+  $( ".horizontal-line" ).animate({
+    opacity: 1,
+    width: "270"
+  }, 1400, function() {
+    // Animation complete.
+  });
+}
+var animateHeader = function (element) {
+  $( element ).animate({
+    opacity: .5,
+    letterSpacing: "2"
+  }, 1000, function() {
+    // Animation complete.
+  });
+}
