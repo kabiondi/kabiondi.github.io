d45be9eade6b90fc3c8fd3878e24d82c98c73632
kabiondi <kellenbiondi@gmail.com>
2017-07-25 14:22:09 -0700

    animates page title both ways

--- portfolio/js/portfolio.js
+++ portfolio/js/portfolio.js
@@ -7,6 +7,7 @@ var animateLine = function () {
     // Animation complete.
   });
 }
+
 var animateHorizontal = function () {
   $( ".horizontal-line" ).animate({
     opacity: 1,
@@ -15,11 +16,21 @@ var animateHorizontal = function () {
     // Animation complete.
   });
 }
+
+var fadeOutHeader = function (element) {
+  $( element ).animate({
+    opacity: 0,
+    letterSpacing: "5px"
+  }, 500, function() {
+    // Animation complete
+  });
+}
+
 var animateHeader = function (element) {
   $( element ).animate({
     opacity: .5,
     letterSpacing: "2"
   }, 1000, function() {
-    // Animation complete.
+    // Animation complete
   });
 }
